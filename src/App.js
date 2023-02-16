aedfffc8554eb02e9c986cf991542b25f29de86a
kel89 <kel89@cornell.edu>
2023-02-16 14:24:56 -0700

    starting on projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 // This one is sick: https://github.com/Sridhar-C-25/react_portfolio_2
 
@@ -12,6 +13,7 @@ function App() {
             <Navbar />
             <Hero />
             <Skills />
+            <Projects />
         </>
     )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Projects({}){
+
+    return (
+        <section id='projects' className='min-h-fit'>
+            <div className='md-container px-5 py-14'>
+                <h2 className='title' data-aos='fade-down'>
+                    Projects
+                </h2>
+                <h4 className='subtitle' data-aos='fade-down'>
+                    Fun things I'm working on
+                </h4>
+            </div>
+        </section>
+    )
+}
